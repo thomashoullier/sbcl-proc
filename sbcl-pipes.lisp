dd7b2d74ea45e427bb09e287dd1787ecf03aff17
karl <alfadr@protonmail.com>
2018-07-08 12:51:04 +0200

    successfully launched gnuplot

--- sbcl-pipes.lisp
+++ sbcl-pipes.lisp
@@ -1,2 +1,12 @@
 ;;;; Wrapping some of the SBCL particular functions to deal broadly with piping to external programs.
 
+(defun run-prog (name &optional (arg nil))
+  "Wraps around sb-ext:run-program mainly to avoid manually setting streams each
+ time. Returns the process outputted by sb-ext:run-program.
+name : Program name, as a string. Eg. 'gnuplot'. Search is done in the $PATH
+arg : Flags provided to the program. As one string."
+  (sb-ext:run-program name (list arg)
+                      :input :stream
+                      :output :stream
+                      :wait nil
+                      :search T))
--- tests.lisp
+++ tests.lisp
@@ -1,2 +1,9 @@
 ;;;; Testing the functions in cl-sbcl-pipes and examples.
-;;;;
+
+
+(defparameter *gp-proc* (run-prog "gnuplot" "-persist"))
+
+(princ #\Newline (sb-ext:process-input *gp-proc*))
+(princ "plot sin(x)" (sb-ext:process-input *gp-proc*))
+(princ #\Newline (sb-ext:process-input *gp-proc*))
+(finish-output (sb-ext:process-input *gp-proc*))
